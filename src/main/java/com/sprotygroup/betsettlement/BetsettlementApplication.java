d277b564d33ee81ad09a8c2285ec1b9bffef2f06
sergheiberber <sberber@complexica.com>
2026-02-11 14:54:41 +0200

    Init commit. Create project skeleton.

---
+++ src/main/java/com/sprotygroup/betsettlement/BetsettlementApplication.java
@@ -0,0 +1,13 @@
+package com.sprotygroup.betsettlement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BetsettlementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BetsettlementApplication.class, args);
+	}
+
+}
